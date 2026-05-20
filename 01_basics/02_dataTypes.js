d50f441d84702d327c8714394316476ad0efa215
Saroj Neupane <sarojneupaneb8@gmail.com>
2026-05-20 15:52:06 +0000

    dataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "Saroj"
+let age = 24
+let islogggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// Null => standalone value
+// undefined 
+// Symbol => unique value
+
+
+// Object
+// Array
+// Function
+// Date
+// RegExp
+// Set 
+// Map
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
+
+
