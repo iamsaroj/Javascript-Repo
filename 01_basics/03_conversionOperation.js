0733cefa6cfb185c9642ab2f4c40e1c9822c310e
Saroj Neupane <sarojneupaneb8@gmail.com>
2026-05-21 02:06:23 +0000

    conversionOperation in js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "saroj"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "55abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "saroj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "Saroj" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
