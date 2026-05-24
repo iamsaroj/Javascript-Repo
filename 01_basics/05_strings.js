82efda264f0e8551bf330edc20378e881f83b933
Saroj Neupane <sarojneupaneb8@gmail.com>
2026-05-24 08:13:06 +0000

    strings in js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,48 +13,51 @@ console.log(gameName.__proto__);
 console.log(gameName.length); // 8
 
 // String Methods Without Arguments
+const gameName = new String(" Saroj  ")
 
 .toLowerCase()
-// console.log(gameName.toLowerCase());
+console.log(gameName.toLowerCase());
 
 .toUpperCase()
-// console.log(gameName.toUpperCase());
+console.log(gameName.toUpperCase());
 
 .trim()
-// console.log(gameName.trim());
+console.log(gameName.trim());
 
 .trimStart()
-// console.log(gameName.trimStart());
+console.log(gameName.trimStart());
 
 .trimEnd()
-// console.log(gameName.trimEnd());
+console.log(gameName.trimEnd());
 
 .toString()
-// console.log(gameName.toString());
+console.log(gameName.toString());
 
 .valueOf()
-// console.log(gameName.valueOf());
+console.log(gameName.valueOf());
 
 
 // String Methods With Arguments
+const gameName = new String(" Saroj  ")
 
 .includes()
-// console.log(gameName.includes("Saroj")); // true
+console.log(gameName.includes("Saroj")); // true
 
 .indexOf()
-// console.log(gameName.indexOf("a"));
+console.log(gameName.indexOf("a"));
 
 .lastIndexOf()
-// console.log(gameName.lastIndexOf("o"));
+console.log(gameName.lastIndexOf("o"));
 
 .startsWith()
-// console.log(gameName.startsWith(" Sar"));
+console.log(gameName.startsWith(" Sar"));
 
 .endsWith()
-// console.log(gameName.endsWith("j  "));
+console.log(gameName.endsWith("j  "));
 
 
 // For Extracting Parts of a String
+const gameName = new String(" Saroj  ")
 
 .slice(); // Extracts a section of a string from the start index up to (but not including) the end index.
 console.log(gameName.slice(1,6)); // (startIndex, endIndex)
@@ -70,6 +73,7 @@ console.log(gameName.charCodeAt(1));
 
 
 // For Modifying & Splitting Text
+const gameName = new String(" Saroj  ")
 
 .replace(); // Replaces the first match of a value with a new value.
 console.log(gameName.replace(" Saroj  ","Developer"));
@@ -86,13 +90,13 @@ console.log(gameName.concat("Neupane")); // gameName${Saroj}.concat ${Neupane} /
 .repeat(); // Returns a new string containing the original string repeated a specific number of times.
 console.log(gameName.repeat(3));
 
-
+const cardExtension = new String('4321');
 .padStart(); // Pads the current string from the start with a given string until it hits the target length.
-console.log(gameName.padStart());
+console.log(`padStart():      ${cardExtension.padStart(16, '*')}`); // "************4321"
 
 .padEnd(); // Pads the current string from the end with a given string until it hits the target length.
-console.log(gameName.padEnd());
+console.log(`padEnd():        ${cardExtension.padEnd(8, '0')}`);   // "43210000"
 
 
-// Note: Crucial Rule to Remember;
-In JavaScript, strings are immutable. None of these methods actually change the original variable; they always return a brand-new string!
+Note: Crucial Rule to Remember: 
+// In JavaScript, strings are immutable. None of these methods actually change the original variable; they always return a brand new string!
